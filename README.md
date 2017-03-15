f234a2672138aeac98b79c71f050c15dcc3e6ddb
Sergiu Tomsa <sergiu.tomsa@shopwings.com>
2017-03-15 09:37:02 +0100

    Structure prototype

---
+++ README.md
@@ -0,0 +1,3 @@
+Add Twitter Bootstrap to Meteor
+===============================
+meteor add twbs:bootstrap@3.3.6
---
+++ client/main.js
@@ -0,0 +1,22 @@
+// Any JS in here is automatically ran for us
+
+// Import the React library
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import ImageList from './components/image_list';
+
+// Create a component
+const App = () => {
+    return (
+        <div>
+            <ImageList />
+        </div>
+    );
+};
+
+// Render this component on the screen
+Meteor.startup(() => {
+    ReactDOM.render(<App />, document.querySelector('.container'));
+    axios.get('https://api.imgur.com/3/gallery/hot/viral/0').then(response => console.log(response));
+});
