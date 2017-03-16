9e4554d63559d4159dc4c59eeec45a9dc0e8ba19
Sergiu Tomsa <sergiu.tomsa@shopwings.com>
2017-03-16 15:37:30 +0100

    Implemented score and general refactoring

--- client/components/image_list.js
+++ client/components/image_list.js
@@ -3,15 +3,11 @@
 import React from 'react';
 import ImageDetail from './image_detail';
 
-const IMAGES = [
-    { title: 'Pen', link: 'http://dummyimage.com/600x400' },
-    { title: 'Pine Tree', link: 'http://dummyimage.com/600x400' },
-    { title: 'Mug', link: 'http://dummyimage.com/600x400' }
-];
-
 //Create our component
-const ImageList = () => {
-    const RenderedImages = IMAGES.map(image =>
+const ImageList = (props) => {
+    const validImages = props.images.filter(image => !image.is_album);
+
+    const RenderedImages = validImages.map(image =>
         <ImageDetail key={image.title} image={image} />
     );
 
--- client/main.js
+++ client/main.js
@@ -1,22 +1,34 @@
 // Any JS in here is automatically ran for us
 
 // Import the React library
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import ImageList from './components/image_list';
 
 // Create a component
-const App = () => {
-    return (
-        <div>
-            <ImageList />
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { images: []};
+    }
+
+    componentWillMount() {
+        axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
+            .then(response => this.setState({ images: response.data.data }));
+    }
+
+    render () {
+        return (
+            <div>
+                <ImageList images={this.state.images} />
+            </div>
+        );
+    }
 };
 
 // Render this component on the screen
 Meteor.startup(() => {
     ReactDOM.render(<App />, document.querySelector('.container'));
-    axios.get('https://api.imgur.com/3/gallery/hot/viral/0').then(response => console.log(response));
 });
